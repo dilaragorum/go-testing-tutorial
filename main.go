fc77b6b0b97757e280e484d21ff943961ffea483
dilaragorum <dilaragorum@gmail.com>
2022-04-06 17:48:45 +0300

    implementation

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func Greeting() string {
+	return "Hello"
+}
+
+func GoodBye(name string) string {
+	text := "Goodbye, " + name + "..."
+	return text
+}
+
+func Add(a int, b int) int {
+	return a + b
+}
+
+func main() {
+	http.HandleFunc("/", GreetingAPI)
+	http.ListenAndServe(":3000", nil)
+}
+
+func GreetingAPI(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello Gopher")
+}
